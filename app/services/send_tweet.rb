1cea06672cea72f941aa14fee684107dc339449a
Justine <>
2018-02-15 10:21:18 +0100

    added tweet service

---
+++ app/services/send_tweet.rb
@@ -0,0 +1,23 @@
+class SendTweet
+	def initialize(content)
+		@tweet = content
+	end
+
+	def perform
+		log_in_to_twitter
+		send_tweet(@tweet)
+	end
+
+	def log_in_to_twitter
+		@client = Twitter::REST::Client.new do |config|
+  		config.consumer_key        = ENV['TWITTER_API_KEY']
+  		config.consumer_secret     = ENV['TWITTER_API_SECRET']
+  		config.access_token        = ENV['TWITTER_TOKEN']
+  		config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+		end
+	end
+
+	def send_tweet(tweet)
+		@client.update(tweet)
+	end
+end
